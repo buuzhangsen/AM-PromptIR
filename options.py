25d3966afcf22471275e90fccb08211ec04d542a
buuzhangsen <17633882162@163.com>
2025-02-19 13:57:45 +0800

    options.py

--- options.py
+++ options.py
@@ -17,17 +17,17 @@ parser.add_argument('--patch_size', type=int, default=128, help='patchsize of in
 parser.add_argument('--num_workers', type=int, default=16, help='number of workers.')
 
 # path
-parser.add_argument('--data_file_dir', type=str, default='PromptIR-main/data_dir/',  help='where clean images of denoising saves.')
-parser.add_argument('--denoise_dir', type=str, default='PromptIR-main/data/data/Train/denoise',
+parser.add_argument('--data_file_dir', type=str, default='/data_dir/',  help='where clean images of denoising saves.')
+parser.add_argument('--denoise_dir', type=str, default='/data/data/Train/denoise',
                     help='where clean images of denoising saves.')
-parser.add_argument('--derain_dir', type=str, default='PromptIR-main/data/data/Train/derain',
+parser.add_argument('--derain_dir', type=str, default='/data/data/Train/derain',
                     help='where training images of deraining saves.')
-parser.add_argument('--dehaze_dir', type=str, default='PromptIR-main/data/data/Train/dehazy',
+parser.add_argument('--dehaze_dir', type=str, default='/data/data/Train/dehazy',
                     help='where training images of dehazing saves.')
 parser.add_argument('--output_path', type=str, default="results/", help='output save path')
-parser.add_argument('--ckpt_path', type=str, default="PromptIR-main/ckpt/Denoise/", help='checkpoint save path')
-parser.add_argument("--wblogger",type=str,default="promptir",help = "Determine to log to wandb or not and the project name")
-parser.add_argument("--ckpt_dir",type=str,default="PromptIR-main/ckpt",help = "Name of the Directory where the checkpoint is to be saved")
+parser.add_argument('--ckpt_path', type=str, default="/ckpt/Denoise/", help='checkpoint save path')
+parser.add_argument("--wblogger",type=str,default="am-promptir",help = "Determine to log to wandb or not and the project name")
+parser.add_argument("--ckpt_dir",type=str,default="/ckpt",help = "Name of the Directory where the checkpoint is to be saved")
 parser.add_argument("--num_gpus",type=int,default= 1,help = "Number of GPUs to use for training")
 
 options = parser.parse_args()
