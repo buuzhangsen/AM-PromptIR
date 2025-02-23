bf7926cb7f721e869688564ea6721ef440393b75
buuzhangsen <17633882162@163.com>
2025-02-23 18:02:05 +0800

    Update My_loss.py

--- My_loss.py
+++ My_loss.py
@@ -8,7 +8,6 @@ class CharbonnierLoss(nn.Module):
     def __init__(self, eps=1e-3):
         super(CharbonnierLoss, self).__init__()
         self.eps = eps
-
     def forward(self, x, y):
         diff = x - y
         # loss = torch.sum(torch.sqrt(diff * diff + self.eps))
